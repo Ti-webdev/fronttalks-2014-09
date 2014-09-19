f2d35b17896ac994cf356a5776302867d2d1a0c3
Vasily Mikhaylovsky <ti.webdev@gmail.com>
2014-09-19 12:52:02 +0600

    refactoring test

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,21 +1,25 @@
 describe("1. calc", function() {
   it('seven(times(five()))', function () {
-    var actual = seven(times(five()));
-    actual.should.equal(35);
+    seven(times(five())).should.equal(35);
   });
 
   it('four(plus(nine()))', function () {
-    var actual = four(plus(nine()));
-    actual.should.equal(13);
+    four(plus(nine())).should.equal(13);
   });
 
   it('eight(minus(three()))', function () {
-    var actual = eight(minus(three()));
-    actual.should.equal(5);
+    eight(minus(three())).should.equal(5);
   });
 
   it('six(dividedBy(two()))', function () {
-    var actual = six(dividedBy(two()));
-    actual.should.equal(3);
+    six(dividedBy(two())).should.equal(3);
   });
 });
+
+// describe("2. object path", function() {
+//   it('a.b.e', function () {
+//     var actual = jpath({
+//
+//     });
+//   });
+// });
