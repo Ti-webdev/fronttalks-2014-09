39573044a37875611a3c222315de0162689ab683
Vasily Mikhaylovsky <ti.webdev@gmail.com>
2014-09-19 14:54:47 +0600

    object-path done

--- object-path.js
+++ object-path.js
@@ -1,7 +1,23 @@
 (function() {
   "use strict";
 
-  this.jpath = function() {
-    return 'd';
-  };
+  // решал пару лет назад сам в вакансии яндекса
+  // но спасибо: http://stackoverflow.com/questions/8817394/javascript-get-deep-value-from-object-by-passing-path-to-it-as-string
+
+  function deepFind(obj, path) {
+    var paths = path.split('.')
+      , current = obj
+      , i;
+
+    for (i = 0; i < paths.length; ++i) {
+      if (current[paths[i]] == undefined) {
+        return undefined;
+      } else {
+        current = current[paths[i]];
+      }
+    }
+    return current;
+  }
+
+  this.jpath = deepFind;
 }).call(this);
