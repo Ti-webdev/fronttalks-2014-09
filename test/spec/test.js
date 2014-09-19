f42e532c01695aa954fa3a6a449bb362f6cebe0a
Vasily Mikhaylovsky <ti.webdev@gmail.com>
2014-09-19 15:14:23 +0600

    travis to slow

--- test/spec/test.js
+++ test/spec/test.js
@@ -76,7 +76,7 @@ describe("4. Randomize Objects", function() {
 
 describe("5. Factorions", function() {
   it('get all factorion', function () {
-    this.timeout(5000);
+    this.timeout(25000);
     // see https://ru.wikipedia.org/wiki/Факторион
     getFactorions().should.eql([1, 2, 145, 40585]);
   });
